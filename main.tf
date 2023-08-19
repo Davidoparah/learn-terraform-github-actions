485c33a88c9cd4d5a15560e3dcf8b3da8b5cee36
David Oparah <davidoparah@gmail.com>
2023-08-19 10:43:52 +0100

    8th commit

--- main.tf
+++ main.tf
@@ -55,9 +55,9 @@ resource "aws_instance" "web" {
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
   // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
